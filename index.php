22f75ccd2f042dadbd4062577eda50b74fae69da
oumaimakasdi <oumaimakasdi@users.noreply.github.com>
2020-07-07 18:54:16 +0100

    new web site

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php include("menu.html"); ?>
+    <?php include("about.html"); ?>
+    <?php include("footer.html"); ?>
+
+  </body>
+</html>
